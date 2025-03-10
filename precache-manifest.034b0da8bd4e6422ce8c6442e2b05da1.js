fd0503a5d02d44c71273bdd9501e1c667da74304
vvh24 <valeriahcrespo@hotmail.com>
2025-03-10 14:41:18 -0400

    Updates

---
+++ precache-manifest.034b0da8bd4e6422ce8c6442e2b05da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346da378caeac6c01b6c0038f5eb8857",
+    "url": "/UFO/index.html"
+  },
+  {
+    "revision": "b57e8f13ebd49110b73c",
+    "url": "/UFO/static/css/main.22fa1222.chunk.css"
+  },
+  {
+    "revision": "e41745a70c96492dc949",
+    "url": "/UFO/static/js/2.e36f0c12.chunk.js"
+  },
+  {
+    "revision": "a734611c61edbd8959bc",
+    "url": "/UFO/static/js/3.7b0de3fe.chunk.js"
+  },
+  {
+    "revision": "b57e8f13ebd49110b73c",
+    "url": "/UFO/static/js/main.d2239525.chunk.js"
+  },
+  {
+    "revision": "73dbff729e0e7b1b9efc",
+    "url": "/UFO/static/js/runtime~main.8c1c7444.js"
+  }
+]);
